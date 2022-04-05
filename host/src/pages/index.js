82ff1324238f4e067f0deb8d2b2ba2b20405f27a
Gaurav Tiwari <gautiwar6@publicisgroupe.net>
2022-04-05 20:28:38 +0530

    fix: changed link name

--- host/src/pages/index.js
+++ host/src/pages/index.js
@@ -1,19 +1,9 @@
 import Load from "../../loader";
 import Link from "next/link";
 import { useState } from "react";
-// import dynamic from "next/dynamic"
-// const Component = Load("remote1","./widget")
-// const Component3 = Load("remote1","./child")
+
 const Component2 = Load("remote2","pagination")
-// Load("remote1","./widget")
-// const Component = dynamic(()=>{
-//   if(typeof window!=='undefined')
-//  return window.remote1.get('./widget').then((f)=>f())
-// },{ssr:false})
-// let Component = (<div></div>)
-// if(typeof window!=='undefined')
-//  Component = window.remote1.get('./widget').then((f)=>f())
-// const  Component = dynamic(()=>import("remote1/widget"))
+
 
 export default function Home({ data }) {
  const [count,setCount] =  useState(0)
@@ -21,9 +11,7 @@ export default function Home({ data }) {
     <div>
       Next
       <div id="container"></div>
-      {/* <Component count={4}/> */}
-      {/* <Component3/> */}
-      <Link href="/test"><a>GOdfdsfs</a></Link>
+      <Link href="/test"><a>widget</a></Link>
       <div>
       <Component2/>
       <button onClick={()=>setCount(count+1)}>changestate{count}</button>
